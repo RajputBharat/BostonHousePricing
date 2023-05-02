e577415ac0142dde83af46c515807a07b785d6ee
Bharat Rajput <bharat.premsing.rajpoot@gmail.com>
2023-05-02 21:46:48 +0530

    app.py debugging

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import pandas
+import numpy as np
 app = Flask(__name__)
 
-model = pickle.load(open('regmodel.pkl','rb'))
-
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -16,6 +17,7 @@ def predict_api():
     print(np.array(list(data.values())).reshape(1,-1))
     new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
-    return jsonify((output[0]))
+    print(output[0])
+    return jsonify(output[0])
 if __name__=="__main__":
     app.run(debug=True)
